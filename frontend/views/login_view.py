eee5b19929a6f6445ba1a8bdac56444d17c651de
fdevmx <filesforcloud.fdev.mx@gmail.com>
2025-04-01 03:17:38 -0600

    feat: Enhance login and registration views with validation

--- frontend/views/login_view.py
+++ frontend/views/login_view.py
@@ -1,13 +1,16 @@
 import streamlit as st
 from frontend.components.form_components import text_input_custom, button_custom
+from backend.auth.auth_service import login
+import re
+from email_validator import validate_email, EmailNotValidError
 
 def login_view():
     # Crear un diseño de 3 columnas y usar solo la columna central
     col1, col2, col3 = st.columns([1, 2, 1])
-    
+
     with col2:  # Solo usar la columna central (más ancha)
         # st.markdown('<div class="auth-container">', unsafe_allow_html=True)
-        st.markdown("## Inicio de sesión")
+        st.title("Iniciar Sesión")
         st.write("")
 
         # Campos del formulario
@@ -16,13 +19,31 @@ def login_view():
 
         # Botón de Iniciar Sesión
         if button_custom("INICIAR SESIÓN"):
-            # Aquí se podría llamar a la lógica de autenticación en la carpeta backend
-            st.write("Lógica de autenticación pendiente...")
+            # Validación básica
+            if not email or not password:
+                st.error("Todos los campos son obligatorios")
+            else:
+                # Importar y llamar a la función de registro
+                if login(email, password):
+                    st.success("Inicio de sesión exitoso!")
+                    st.session_state.vista = "private_general"
+                    st.rerun()
+                else:
+                    st.error("Correo o contraseña incorrectos")
 
         st.write("")
+
+        col1, col2 = st.columns([1, 1])
         
-        # Link para registro (ahora con botón)
-        if button_custom("¿No tienes cuenta? Regístrate"):
-            st.session_state.vista = "register"
-            st.rerun()
-        st.markdown('</div>', unsafe_allow_html=True)
+        with col1:
+            # Link para registro (ahora con botón)
+            if button_custom("¿No tienes cuenta? Regístrate"):
+                st.session_state.vista = "register"
+                st.rerun()
+            st.markdown('</div>', unsafe_allow_html=True)
+
+        with col2:
+            # Link para volver a la página principal
+            if button_custom("Volver a la página principal"):
+                st.session_state.vista = "public_apps"
+                st.rerun()
